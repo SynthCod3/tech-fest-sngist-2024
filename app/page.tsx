2600374586335a71c0d89a491a6bbdf6e96ed4d1
Jenin82 <jenin8282@gmail.com>
2024-01-15 11:38:58 +0530

    shadcn implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
 
 export default function Home() {
     return (
         <>
             <h1>test</h1>
+			<ModeToggle />
         </>
     );
 }
---
+++ components/ui/ModeToggle.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+    const { setTheme } = useTheme();
+
+    return (
+        <Button variant="outline" size="icon">
+            <SunIcon
+                className="h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100"
+                onClick={() => setTheme("light")}
+            />
+            <MoonIcon
+                className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-90 transition-all dark:rotate-0 dark:scale-0"
+                onClick={() => setTheme("dark")}
+            />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    );
+}

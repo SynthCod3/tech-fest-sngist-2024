03b2763950353eb7f4d8e2d4178b55f5ce1b5661
Amal C P <acp070146@gmail.com>
2024-03-16 17:42:32 +0530

    partners section added

--- src/modules/Home/components/Partners/Partners.tsx
+++ src/modules/Home/components/Partners/Partners.tsx
@@ -1,6 +1,7 @@
 import styles from './Partners.module.css'
 import fundesign from './assets/fundesign.png'
 import creative from './assets/Creartive.png'
+import mulearn from './assets/µLearn.png'
 
 type Props = {}
 
@@ -12,6 +13,9 @@ export const Partners = (_props: Props) => {
       </a>{" "}
       <a href="https://fundesign.in" target="_blank">
         <img src={creative} alt="" />
+      </a>
+      <a href="https://mulearn.org" target="_blank">
+        <img src={mulearn} alt="" />
       </a>
     </div>
   );
